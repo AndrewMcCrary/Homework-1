05e4d088b12acf47e617d85f52f10502e7d823d9
AndrewMcCrary <ajmccrary03@gmail.com>
2022-01-30 13:59:19 -0500

    additions

--- Homework 1/main.cpp
+++ Homework 1/main.cpp
@@ -12,56 +12,92 @@ struct Book {
 	string title;
 	string author;
 	int wordCount;
-	float letterFreq[26];
+	int letterFreq[26];
 	int newLineCount;
 };
 
 int main(void) {
 	
 	string filename;
-	ifstream fstr;
+	ifstream lineFstrstr;
+	ifstream wordFstrstr;
 	string line;
-	vector<string> contents;
-	Book b = {NULL, NULL, 0, NULL, 0};
+	vector<string> lineContents;
+	vector<string> strContents;
 
 	do {
 		cout << "Enter the name of the file to be processed: ";
 		cin >> filename;
 		try {
-			fstr.open(filename);
-			if (fstr)
+			lineFstrstr.open(filename);
+			wordFstrstr.open(filename);
+			if (lineFstrstr)
 				break;
 		}
 		catch (exception e) {
 			cout << e.what() << endl;
 		}
 		cout << "File does not exist." << endl << endl;
-	} while (!fstr);
+	} while (!lineFstrstr);
 
 	Book b;
 	b.wordCount = 0;
 	b.newLineCount = 0;
 
-	getline(fstr, line);
+	getline(lineFstrstr, line);
 	b.title = line;
 
-	getline(fstr, line);
+	getline(lineFstrstr, line);
 	b.author = line;
 
-	while (getline(fstr, line)) {
+	while (getline(lineFstrstr, line)) {
 		if (line.compare(0, 8, "Contents") == 0)
 			break;
 	}
 
-	fstr >> line;
-	while (fstr) {
-		b.wordCount++;
-		contents.push_back(line);
-		fstr >> line;
+	while (getline(wordFstrstr, line)) {
+		if (line.compare(0, 8, "Contents") == 0)
+			break;
 	}
 
+	getline(lineFstrstr, line);
+	while (lineFstrstr) {
+		b.newLineCount++;
+		lineContents.push_back(line);
+		getline(lineFstrstr, line);
+	}
 
-	cout << contents.size() << endl;
+	wordFstrstr >> line;
+	while (wordFstrstr) {
+		b.wordCount++;
+		strContents.push_back(line);
+		wordFstrstr >> line;
+	}
+
+	// Save the info in cardcatalog.txt
+	ofstream output("CardCatalog.txt", std::ios_base::app | std::ios_base::out);
+	output << "Title: " << b.title << endl;
+	output << "Full Author: " << b.author << endl;
+
+	int pos = b.author.find(' ');
+	output << "Author First Name: " << b.author.substr(0, pos) << endl;
+	output << "Author Last Name: " << b.author.substr(pos + 1, b.author.size()) << endl;
+
+	output << "Word Count: " << b.wordCount << endl;
+	output << "Line Count: " << b.newLineCount << endl;
+
+	cout << lineContents.size() << endl;
+	cout << strContents.size() << endl;
+
+	for (int i = 0; i < sizeof(b.letterFreq) / sizeof(int); i++) {
+		b.letterFreq[i] = 0;
+	}
+
+	for (int i = 0; i < strContents.size(); i++) {
+		for (int j = 0; j < strContents[j].size(); j++) {
+			b.letterFreq[LALPHA.find(tolower(strContents[i][j]))]++;
+		}
+	}
 
 	system("pause");
 	return 0;
